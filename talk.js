f84628e42772fb76455b65c58c42a9716b749cb6
ROBOLINK <110363534+ROBOLINK0@users.noreply.github.com>
2023-04-06 12:58:35 +0900

    Add files via upload

--- talk.js
+++ talk.js
@@ -21,69 +21,263 @@ document.addEventListener("click", (event) => {
 });
 
 const check = () => {
+
+  // 로이랑 놀기 ---------------------------------------------------------------------------------------------------------
   if (inMsg.includes("안녕")) {
     document.getElementById("output").innerText = "안녕하세요 ^^";
+    document.getElementById("bot_img").style.background =
+        "url(./img/ROE_Hi.png) no-repeat";
   } 
+
   else if (inMsg.includes("이름")) {
-    document.getElementById("output").innerText = "안녕하세요! 저는 로이라고 합니다.";} 
+    document.getElementById("output").innerText = "안녕하세요! 저는 로보링크의 챗 봇 \n \"로이\"라고 합니다.";
+    document.getElementById("bot_img").style.background =
+        "url(./img/ROE_NAME.png) no-repeat";
+  } 
+
+  else if (inMsg.includes("바보")||inMsg.includes("멍청이")) {
+    document.getElementById("output").innerText = "더 열심히 노력하겠습니다. ㅠㅠ";
+    document.getElementById("bot_img").style.background =
+        "url(./img/ROE_nn.png) no-repeat";
+  } 
+
+  else if (inMsg.includes("미안")) {
+    document.getElementById("output").innerText = "괜찮습니다! ^^";
+    document.getElementById("bot_img").style.background =
+        "url(./img/ROE_n.png) no-repeat";
+  } 
+
+  else if (inMsg.includes("로이")) {
+    document.getElementById("output").innerText = "저를 부르셨나요?";
+    document.getElementById("bot_img").style.background =
+        "url(./img/ROE_n.png) no-repeat";
+  } 
+
+  else if ((inMsg.includes("사랑"))||inMsg.includes("고마워")||inMsg.includes("감사"))  {
+    document.getElementById("output").innerText = "행복한 말은 저에게 힘이 됩니다! ♥♥♥";
+    document.getElementById("bot_img").style.background =
+        "url(./img/ROE_love.png) no-repeat";
+  } 
+  
+
+// 코드론 미니 ---------------------------------------------------------------------------------------------------------
+
   else if (
-    (inMsg.includes("주미") && inMsg.includes("코드론")) || inMsg.includes("드론")) {
-    let random = Math.floor(Math.random() * 3) + 1;
-    switch (random) {
-      case 1:
-        document.getElementById("output").innerText = "네? 뭐라고 하셨어요?";
-        break;
-      case 2:
-        document.getElementById("output").innerText = "??????????";
-        break;
-      case 3:
-        document.getElementById("output").innerText = "넵!!";
-        document.body.style.background = "black";
-        document.getElementById("user_msg").style.color = "white";
-        break;
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("이상") ) {
+      document.getElementById("output").innerText = "코드론 미니가 이상하게 날아 갈 경우 \n트림을 추천 드립니다!\n 모터의 출력을 조절해주는 미세조정(트림)을 합니다.";
+      //document.body.style.background = "rgb(255, 242, 204, 1)" 
+      document.getElementById("bot_img").style.background =
+      "url(./img/Mini_Trim.png) no-repeat";
     }
-  } else if (inMsg.includes("불") && inMsg.includes("켜")) {
-    document.getElementById("output").innerText = "불 켜져랏~ 얍-!!";
-    document.getElementById("bot_img").style.background =
-      "url(./img/lightOn.png) no-repeat";
-    document.getElementById("bot_img").style.backgroundSize = "contain";
-    setTimeout(() => {
-      document.body.style.background = "white";
-      document.getElementById("bot_img").style.background =
-        "url(./img/lightOnDone.png) no-repeat";
-      document.getElementById("bot_img").style.backgroundSize = "contain";
-      document.getElementById("user_msg").style.color = "black";
-    }, 1000);
-  } else if (inMsg.includes("바보")) {
-    document.getElementById("output").innerText = "ㅠㅠ";
-    document.getElementById("bot_img").style.background =
-      "url(./img/iwont.png) no-repeat";
-    document.getElementById("bot_img").style.backgroundSize = "contain";
-    setTimeout(() => {
-      document.getElementById("bot_img").style.background =
-        "url(./img/iwontDone.png) no-repeat";
-      document.getElementById("bot_img").style.backgroundSize = "contain";
-    }, 400);
-  } else if (inMsg.includes("미안")) {
-    document.getElementById("output").innerText = "괜찮습니다!";
-    document.getElementById("bot_img").style.background =
-      "url(./img/default.png) no-repeat";
-    document.getElementById("bot_img").style.backgroundSize = "contain";
-  } else if (inMsg.includes("로이")) {
-        document.getElementById("output").innerText = "저에 관한것은 비밀이랍니다! ^^";
+
+  else if (
+      (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("흘러") ) {
+        document.getElementById("output").innerText = "코드론 미니가 이상하게 날아 갈 경우 \n트림을 추천 드립니다!\n 모터의 출력을 조절해주는 미세조정(트림)을 합니다.";
+        //document.body.style.background = "rgb(255, 242, 204, 1)" 
         document.getElementById("bot_img").style.background =
-          "url(./img/default.png) no-repeat";
-        document.getElementById("bot_img").style.backgroundSize = "contain";
-  } else if (inMsg.includes('고마워')) {
-    document.getElementById("output").innerText =
-      "고마움의 표현은 입금으로 ♥_♥";
+        "url(./img/Mini_Trim.png) no-repeat";
+      }
+  else if (
+        (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("한쪽") ) {
+          document.getElementById("output").innerText = "코드론 미니가 이상하게 날아 갈 경우 \n트림을 추천 드립니다!\n 모터의 출력을 조절해주는 미세조정(트림)을 합니다.";
+          //document.body.style.background = "rgb(255, 242, 204, 1)" 
+          document.getElementById("bot_img").style.background =
+          "url(./img/Mini_Trim.png) no-repeat";
+        }
+  else if (
+          (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("한 쪽") ) {
+            document.getElementById("output").innerText = "코드론 미니가 이상하게 날아 갈 경우 \n트림을 추천 드립니다!\n 모터의 출력을 조절해주는 미세조정(트림)을 합니다.";
+            //document.body.style.background = "rgb(255, 242, 204, 1)" 
+            document.getElementById("bot_img").style.background =
+            "url(./img/Mini_Trim.png) no-repeat";
+          }
+
+
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("선") ) {
+      document.getElementById("output").innerText = "코드론 미니의 끊어진 선은 안테나 입니다.";
+      document.getElementById("bot_img").style.background =
+      "url(./img/Mini_line.png) no-repeat";
+    }
+ // 코드론 미니 소음  ---------------------------------------------------------------------------------------------------------
+   else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("소음") ) {
+    document.getElementById("output").innerText = "코드론 미니의 소리나 소음은 \n 아래의 이미지를 참고해주세요!";
+    //document.body.style.background = "rgb(255, 242, 204, 1)" 
     document.getElementById("bot_img").style.background =
-      "url(./img/twinkle.png) no-repeat";
-    document.getElementById("bot_img").style.backgroundSize = "contain";
-  } else {
-    document.getElementById("output").innerText = "잘 모르겠어요!";
+    "url(./img/Mini_db.png) no-repeat";
+  }
+
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("소리") ) {
+    document.getElementById("output").innerText = "코드론 미니의 소리나 소음은 \n 아래의 이미지를 참고해주세요!";
+    //document.body.style.background = "rgb(255, 242, 204, 1)" 
     document.getElementById("bot_img").style.background =
-      "url(./img/what.png) no-repeat";
+    "url(./img/Mini_db.png) no-repeat";
+  }
+
+  // 코드론 미니 조종기  ---------------------------------------------------------------------------------------------------------
+
+  else if (
+      (inMsg.includes("코드론") && inMsg.includes("미니") && inMsg.includes("조종기")) || inMsg.includes("버튼") || inMsg.includes("눌림")) {
+        document.getElementById("output").innerText = "코드론 조종기의 버튼이 눌리지 않는다면 \n 아래의 이미지를 참고해주세요!";
+        document.getElementById("bot_img").style.background =
+        "url(./img/Mini_controller.png) no-repeat";
+      }
+
+  else if (
+        (inMsg.includes("코드론") && inMsg.includes("미니") && inMsg.includes("컨트롤러")) || inMsg.includes("버튼") || inMsg.includes("눌림")) {
+          document.getElementById("output").innerText = "코드론 조종기의 버튼이 눌리지 않는다면 \n 아래의 이미지를 참고해주세요!";
+          document.getElementById("bot_img").style.background =
+          "url(./img/Mini_controller.png) no-repeat";
+        }
+
+  else if (
+          (inMsg.includes("코드론") && inMsg.includes("미니") && inMsg.includes("조종기")) || inMsg.includes("리셋") || inMsg.includes("Reset") || inMsg.includes("RESET") || inMsg.includes("버튼")) {
+            document.getElementById("output").innerText = "코드론 미니를 초기화 하는 방법은 \n 아래의 이미지를 참고해주세요!";
+            document.getElementById("bot_img").style.background =
+            "url(./img/Mini_reset.png) no-repeat";
+          }
+  
+  else if (
+            (inMsg.includes("코드론") && inMsg.includes("미니") && inMsg.includes("컨트롤러")) || inMsg.includes("리셋") || inMsg.includes("Reset") || inMsg.includes("RESET") || inMsg.includes("버튼")) {
+              document.getElementById("output").innerText =  "코드론 미니를 초기화 하는 방법은 \n 아래의 이미지를 참고해주세요!";
+              document.getElementById("bot_img").style.background =
+              "url(./img/Mini_reset.png) no-repeat";
+            }  
+
+  else if (
+            (inMsg.includes("코드론") && inMsg.includes("미니") && inMsg.includes("리셋")) || inMsg.includes("Reset") || inMsg.includes("RESET") || inMsg.includes("버튼")) {
+              document.getElementById("output").innerText =  "코드론 미니를 초기화 하는 방법은 \n 아래의 이미지를 참고해주세요!";
+              document.getElementById("bot_img").style.background =
+              "url(./img/Mini_reset.png) no-repeat";
+            }  
+
+  else if (
+              (inMsg.includes("코드론") && inMsg.includes("미니") && inMsg.includes("초기화")) || inMsg.includes("Reset") || inMsg.includes("RESET") || inMsg.includes("버튼")) {
+                document.getElementById("output").innerText =  "코드론 미니를 초기화 하는 방법은 \n 아래의 이미지를 참고해주세요!";
+                document.getElementById("bot_img").style.background =
+                "url(./img/Mini_reset.png) no-repeat";
+              }  
+
+  else if (
+                (inMsg.includes("코드론") && inMsg.includes("미니") && inMsg.includes("헤드리스"))) {
+                  document.getElementById("output").innerText =  "코드론 미니의 헤드리스 모드 설명은 \n 아래의 이미지를 참고해주세요!";
+                  document.getElementById("bot_img").style.background =
+                  "url(./img/Mini_headless.png) no-repeat";
+                }  
+
+  else if (
+                (inMsg.includes("코드론") && inMsg.includes("미니") && inMsg.includes("head"))) {
+                  document.getElementById("output").innerText =  "코드론 미니의 헤드리스 모드 설명은 \n 아래의 이미지를 참고해주세요!";
+                  document.getElementById("bot_img").style.background =
+                  "url(./img/Mini_headless.png) no-repeat";
+                  }  
+  // 코드론 미니 이륙 안될때 ---------------------------------------------------------------------------------------------------------
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("이륙")) {
+      document.getElementById("output").innerText = "코드론 미니가 이륙하지 않는다면 \n 아래의 이미지를 참고해주세요!";
+      document.getElementById("bot_img").style.background =
+      "url(./img/Mini_x.png) no-repeat";
+    }
+
+  else if (
+      (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("안")) {
+        document.getElementById("output").innerText = "코드론 미니가 이륙하지 않는다면 \n 아래의 이미지를 참고해주세요!";
+        document.getElementById("bot_img").style.background =
+        "url(./img/Mini_x.png) no-repeat";
+      }
+
+  else if (
+        (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("날아")) {
+          document.getElementById("output").innerText = "코드론 미니가 이륙하지 않는다면 \n 아래의 이미지를 참고해주세요!";
+          document.getElementById("bot_img").style.background =
+          "url(./img/Mini_x.png) no-repeat";
+        }
+
+  else if (
+          (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("이륙")) {
+            document.getElementById("output").innerText = "코드론 미니가 이륙하지 않는다면 \n 아래의 이미지를 참고해주세요!";
+            document.getElementById("bot_img").style.background =
+            "url(./img/Mini_x.png) no-repeat";
+          }
+
+
+
+ // 코드론 미니 스펙/ 성능 안될때 ---------------------------------------------------------------------------------------------------------
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("스펙")) {
+    document.getElementById("output").innerText = "코드론 미니의 상세 성능은 \n 아래의 이미지를 참고해주세요!";
+    document.getElementById("bot_img").style.background =
+    "url(./img/Mini_spac.png) no-repeat";
+  }
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("성능")) {
+    document.getElementById("output").innerText = "코드론 미니의 상세 성능은 \n 아래의 이미지를 참고해주세요!";
+    document.getElementById("bot_img").style.background =
+    "url(./img/Mini_spac.png) no-repeat";
+  }
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("spac")) {
+    document.getElementById("output").innerText = "코드론 미니의 상세 성능은 \n 아래의 이미지를 참고해주세요!";
+    document.getElementById("bot_img").style.background =
+    "url(./img/Mini_spac.png) no-repeat";
+  }
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("비행시간")) {
+    document.getElementById("output").innerText = "코드론 미니의 상세 성능은 \n 아래의 이미지를 참고해주세요!";
+    document.getElementById("bot_img").style.background =
+    "url(./img/Mini_spac.png) no-repeat";
+  }
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("충전")) {
+    document.getElementById("output").innerText = "코드론 미니의 상세 성능은 \n 아래의 이미지를 참고해주세요!";
+    document.getElementById("bot_img").style.background =
+    "url(./img/Mini_spac.png) no-repeat";
+  }
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("시간")) {
+    document.getElementById("output").innerText = "코드론 미니의 상세 성능은 \n 아래의 이미지를 참고해주세요!";
+    document.getElementById("bot_img").style.background =
+    "url(./img/Mini_spac.png) no-repeat";
+  }
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("센서")) {
+    document.getElementById("output").innerText = "코드론 미니의 상세 성능은 \n 아래의 이미지를 참고해주세요!";
+    document.getElementById("bot_img").style.background =
+    "url(./img/Mini_spac.png) no-repeat";
+  }
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("통신")) {
+    document.getElementById("output").innerText = "코드론 미니의 상세 성능은 \n 아래의 이미지를 참고해주세요!";
+    document.getElementById("bot_img").style.background =
+    "url(./img/Mini_kg.png) no-repeat";
+  }
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("무게")) {
+    document.getElementById("output").innerText = "코드론 미니의 상세 성능은 \n 아래의 이미지를 참고해주세요!";
+    document.getElementById("bot_img").style.background =
+    "url(./img/Mini_kg.png) no-repeat";
+  }
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("크기")) {
+    document.getElementById("output").innerText = "코드론 미니의 상세 성능은 \n 아래의 이미지를 참고해주세요!";
+    document.getElementById("bot_img").style.background =
+    "url(./img/Mini_spac.png) no-repeat";
+  }
+  else if (
+    (inMsg.includes("코드론") && inMsg.includes("미니")) && inMsg.includes("거리")) {
+    document.getElementById("output").innerText = "코드론 미니의 상세 성능은 \n 아래의 이미지를 참고해주세요!";
+    document.getElementById("bot_img").style.background =
+    "url(./img/Mini_kg.png) no-repeat";
+  }
+
+  // 디폴트 ---------------------------------------------------------------------------------------------------------
+  //디폴트
+  else {
+    document.getElementById("output").innerText = "말씀하신 내용은 잘 모르겠어요! \n\n \"이름\"과 \"연락처\"를 남겨서\n 아래의 메일로 알려주시면\n답변 드리겠습니다!";
+    document.getElementById("bot_img").style.background =
+        "url(./img/ROE_as.png) no-repeat";
     document.getElementById("bot_img").style.backgroundSize = "contain";
   }
 };
